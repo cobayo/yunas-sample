7a56cf48a1387d2b086047dded151d247fe05053
Kobayashi Yosuke <yosuke032@icloud.com>
2017-11-04 15:07:06 +0900

    Add Java

---
+++ src/main/java/sample/Main.java
@@ -0,0 +1,29 @@
+package sample;
+
+import yunas.ModelAndView;
+import yunas.Yunas;
+
+import java.util.HashMap;
+import java.util.Map;
+
+
+public class Main {
+
+
+    public static void main(String[] args) {
+
+
+        // Top Page (Return html with Thymeleaf)
+        Yunas.get("/", context -> {
+            Map<String,String> res = new HashMap<>();
+            ModelAndView modelAndView = new ModelAndView(res,"index");
+            return modelAndView;
+        });
+
+
+
+    }
+
+
+}
+
