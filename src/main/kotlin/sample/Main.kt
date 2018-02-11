7cd467494b2d3e299116081deebbee66a657de0e
Kobayashi Yosuke <yosuke032@icloud.com>
2018-02-11 21:34:23 +0900

    Modify Sample Code

--- src/main/kotlin/sample/Main.kt
+++ src/main/kotlin/sample/Main.kt
@@ -7,20 +7,17 @@ import yunas.ModelAndView
 import yunas.Yunas
 
 
-object Main {
+fun main(args: Array<String>) {
 
-    @JvmStatic
-    fun main(args: Array<String>) {
         // Top Page (Return html with Thymeleaf)
-        Yunas.get("/",TopController())
-    }
+        Yunas.get("/", object : Controller {
+            override fun action(context: Context): Any {
+                return ModelAndView(mapOf("message" to "Hello World"),"index")
+            }
 
-    private class TopController : Controller {
-
-        override fun action(context: Context): Any {
-            return ModelAndView(mapOf("message" to "Hello World"),"index")
-        }
-    }
+        })
 
 }
+
+
 
